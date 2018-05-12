c42007ff94a8cd4d17789f84d5ddbe4c6d185a4a
bkell11 <bkeller11@msn.com>
2018-05-11 23:26:03 -0600

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,12 +16,14 @@
 // if not dead hero inflictdamage(villaincounterdamage)
 
 // check for deaths..
-var Characters = [new Character("thor", "Thor", "assets/images/Thor.jpg", 700, 20, 40), new Character("iron-man", "Iron Man", "assets/images/Iron_Man.jpg", 800, 15, 60), new Character("thanos", "Thanos", "assets/images/Thanos.jpg", 950, 10, 70), new Character("loki", "Loki", "assets/images/Loki.png", 700, 20, 30),];
+var Characters;
 var heroCharacter;
 var villainCharacter;
+var villainsDied = 0;
 var gameOver = false;
 
 $(document).ready(function () {
+    restartGame();
     getHeros();
     $("#reset").hide();
     // create new function to call on ready and reset..
@@ -29,6 +31,9 @@ $(document).ready(function () {
         if (heroCharacter != null && villainCharacter != null) {
             attack();
         }
+    });
+    $("#reset").on("click", function () {
+        restartGame();
     });
 });
 
@@ -88,26 +93,50 @@ function attack() {
     var heroDied = false;
     $("#" + villainCharacter.id + "HP").html("HP: " + villainCharacter.hp);
     if (villainDied) {
-        $("#" + villainCharacter.id).remove();
+        $("#" + villainCharacter.id).hide();
         $("#fight").text("");
-        $("#counter-death").text(heroCharacter.name + " has defeated " + villainCharacter.name + "! Choose another enemy!");
+        $("#counter-death").text(heroCharacter.name + " Has Defeated " + villainCharacter.name + "! Choose Another Enemy!");
         villainCharacter = null;
+        villainsDied++;
+        if (villainsDied == 3) {
+            $("#counter-death").text(heroCharacter.name + " Has Won! Play Again.");
+            $("#reset").show();
+        }
     }
 
     else {
         heroCharacter.procureDamage(villainDamage);
         heroDied = heroCharacter.isDead();
         $("#" + heroCharacter.id + "HP").html("HP: " + heroCharacter.hp);
-        $("#fight").text(heroCharacter.name + " attacked " + villainCharacter.name + " with " + heroCharacter.currentAttackPower + " power.")
-        $("#counter-death").text(villainCharacter.name + " countered attacked " + heroCharacter.name + " with " + villainCharacter.counterAttackPower + " power.")
+        $("#fight").text(heroCharacter.name + " Attacked " + villainCharacter.name + " With " + heroCharacter.currentAttackPower + " Power.")
+        $("#counter-death").text(villainCharacter.name + " Countered Attacked " + heroCharacter.name + " With " + villainCharacter.counterAttackPower + " Power.")
         if (heroDied) {
-            $("#" + heroCharacter.id).remove();
+            $("#" + heroCharacter.id).hide();
             $("#fight").text("");
-            $("#counter-death").text(villainCharacter.name + " has defeated " + heroCharacter.name + "! Game Over! Try again.");
+            $("#counter-death").text(villainCharacter.name + " Has Defeated " + heroCharacter.name + "! Game Over! Try Again.");
             heroCharacter = null;
             gameOver = true;
             $("#reset").show();
         }
+    }
+}
+
+function restartGame() {
+    gameOver = false;
+    heroCharacter = null;
+    villainCharacter = null;
+    villainsDied = 0;
+    Characters = [new Character("thor", "Thor", "assets/images/Thor.jpg", 785, 20, 40), new Character("iron-man", "Iron Man", "assets/images/Iron_Man.jpg", 810, 15, 60), new Character("thanos", "Thanos", "assets/images/Thanos.jpg", 950, 10, 70), new Character("loki", "Loki", "assets/images/Loki.png", 725, 20, 30),];
+    $("#reset").hide();
+
+    for (var i = 0; i < Characters.length; i++) {
+
+        var characterDiv = $("#" + Characters[i].id);
+        characterDiv.show();
+        var heroSelections = $("#hero-selection");
+        heroSelections.append(characterDiv);
+
+        $("#" + Characters[i].id + "HP").html("HP: " + Characters[i].hp);
     }
 }
 
